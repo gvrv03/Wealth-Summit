1b2e70e58e82950f0d735e4f0e5238afce307ce0
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-05-30 09:56:43 +0530

    try 1

--- src/app/Products/Product/[PID]/page.js
+++ src/app/Products/Product/[PID]/page.js
@@ -7,42 +7,48 @@ import ProductDetailTop from "@/Components/Home/Product/ProductDetailTop";
 import BuyBtn from "@/Components/Home/Product/BuyBtn";
 
 const ProductDetal = async ({ searchParams }) => {
-  const res = await axios.get(getSingleProductURL + searchParams.product, {
-    headers: {
-      "Cache-Control": "no-cache",
-    },
-  });
-  const product = await res?.data;
-  const { artical } = product ? product : {};
-  if (artical === undefined) {
-    return (
-      <div className=" bg-gray-950  h-full ">
-        <NotFoundProduct />
-      </div>
-    );
-  }
+  // const res = await axios.get(getSingleProductURL + searchParams.product, {
+  //   headers: {
+  //     "Cache-Control": "no-cache",
+  //   },
+  // });
+  // const product = await res?.data;
+  // const { artical } = product ? product : {};
+  // if (artical === undefined) {
+  //   return (
+  //     <div className=" bg-gray-950  h-full ">
+  //       <NotFoundProduct />
+  //     </div>
+  //   );
+  // }
+  // return (
+  //   <div className=" md:m-2 flex-col flex gap-5 m-0">
+  //     <ProductDetailTop product={product} />
+  //     <div className="flex gap-5 md:flex-row flex-col-reverse">
+  //       <div className="w-full bg-ground border border-gray-700  rounded-sm md:rounded-md    p-5 text-justify ">
+  //         <article
+  //           className=" se-wrapper-inner text-justify  "
+  //           dangerouslySetInnerHTML={{ __html: product?.artical }}
+  //         />
+  //       </div>
+  //       <div className=" hidden md:flex bg-ground md:border  w-full md:w-[25%]  border-gray-700  rounded-sm md:rounded-md   gap-2 flex-col  p-5 text-justify ">
+  //         <BuyBtn
+  //           product={{
+  //             productID: product?.productID,
+  //             id: product?._id,
+  //             name: product?.title,
+  //             price: product?.pricing?.price,
+  //           }}
+  //         />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
   return (
-    <div className=" md:m-2 flex-col flex gap-5 m-0">
-      <ProductDetailTop product={product} />
-      <div className="flex gap-5 md:flex-row flex-col-reverse">
-        <div className="w-full bg-ground border border-gray-700  rounded-sm md:rounded-md    p-5 text-justify ">
-          <article
-            className=" se-wrapper-inner text-justify  "
-            dangerouslySetInnerHTML={{ __html: product?.artical }}
-          />
-        </div>
-        <div className=" hidden md:flex bg-ground md:border  w-full md:w-[25%]  border-gray-700  rounded-sm md:rounded-md   gap-2 flex-col  p-5 text-justify ">
-          <BuyBtn
-            product={{
-              productID: product?.productID,
-              id: product?._id,
-              name: product?.title,
-              price: product?.pricing?.price,
-            }}
-          />
-        </div>
-      </div>
-    </div>
+    <>
+      <div>Test</div>
+    </>
   );
 };
 
