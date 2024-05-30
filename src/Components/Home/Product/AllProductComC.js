e5214ee49aebe2c84a859d8418fc607729e58d53
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-05-30 15:35:42 +0530

    Try solve error 2

--- src/Components/Home/Product/AllProductComC.js
+++ src/Components/Home/Product/AllProductComC.js
@@ -3,14 +3,8 @@ import axios from "axios";
 import React from "react";
 import ProductCard from "./ProductCard";
 
-const AllProductComC = async () => {
-  const res = await axios.get(ProductsURL, {
-    headers: {
-      "Cache-Control": "no-cache",
-    },
-  });
-  const Data = await res?.data;
-  if (Data?.products === undefined) {
+const AllProductComC = ({ products }) => {
+  if (products === undefined) {
     return (
       <div className="h-screen w-full grid place-items-center   bg-gray-950  ">
         Error occuured
@@ -19,13 +13,13 @@ const AllProductComC = async () => {
   }
   return (
     <div>
-      {Data?.products?.length === 0 && (
+      {products?.length === 0 && (
         <div className="w-full text-center font-semibold ">
           No Products Found
         </div>
       )}
       <div className="grid grid-cols-2 gap-5 md:grid-cols-4 ">
-        {Data?.products?.map((item, index) => {
+        {products?.map((item, index) => {
           return (
             <ProductCard
               key={index}
--- src/app/Products/page.js
+++ src/app/Products/page.js
@@ -1,12 +1,20 @@
 import AllProductComC from "@/Components/Home/Product/AllProductComC";
 import AllProductSkeleton from "@/Components/Skeleton/AllProductSkeleton";
+import { ProductsURL } from "@/helper/allLinks";
+import axios from "axios";
 import React from "react";
 import { Suspense } from "react";
 
-const page = () => {
+const page = async () => {
+  const res = await axios.get(ProductsURL, {
+    headers: {
+      "Cache-Control": "no-cache",
+    },
+  });
+  const Data = await res?.data;
   return (
     <Suspense fallback={<AllProductSkeleton />}>
-      <AllProductComC />
+      <AllProductComC products={Data?.products} />
     </Suspense>
   );
 };
