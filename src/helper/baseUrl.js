459c6008373e6d2b892557e6d21ff9d5acc31180
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-06-05 09:44:57 +0530

    Add Phonpe PG 2

--- src/helper/baseUrl.js
+++ src/helper/baseUrl.js
@@ -1,11 +1,11 @@
 let baseUrl = "";
-export let basePhonePeURL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/";
+export let basePhonePeURL = "";
 
 if (process.env.NODE_ENV == "development") {
   baseUrl = "http://localhost:3000/";
-  // basePhonePeURL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/";
+  basePhonePeURL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/";
 } else {
-  baseUrl = "https:/wealthsummitnew.vercel.app/";
-  // basePhonePeURL = " https://api.phonepe.com/apis/hermes/pg/v1/";
+  baseUrl = "https://www.wealthsummit.shop/";
+  basePhonePeURL = " https://api.phonepe.com/apis/hermes/pg/v1/";
 }
 export default baseUrl;
