0a55251f079fa141078ecbe79c5ed0a3e8687dcb
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-06-05 11:04:09 +0530

    Add pG URl 1

--- src/helper/baseUrl.js
+++ src/helper/baseUrl.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV == "development") {
   baseUrl = "http://localhost:3000/";
   // basePhonePeURL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/";
 } else {
-  baseUrl = "https://www.wealthsummit.shop/";
+  baseUrl = "https://wealthsummitnew.vercel.app/";
   // basePhonePeURL = " https://api.phonepe.com/apis/hermes/pg/v1/";
 }
 export default baseUrl;
