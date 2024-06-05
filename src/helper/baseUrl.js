cd551935609ae63d5e8d4cbdd521774a1a73d42d
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-06-05 09:38:41 +0530

    Add Invoice page 4

--- src/helper/baseUrl.js
+++ src/helper/baseUrl.js
@@ -1,5 +1,5 @@
 let baseUrl = "";
-export let basePhonePeURL = "https://api.phonepe.com/apis/hermes/pg/v1/";
+export let basePhonePeURL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/";
 
 if (process.env.NODE_ENV == "development") {
   baseUrl = "http://localhost:3000/";
