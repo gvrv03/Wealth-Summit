2fa64bdc23ffe86782318c28bca2241f6bb58941
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-06-08 12:10:44 +0530

    remove useseefect in product page

--- src/app/Products/page.js
+++ src/app/Products/page.js
@@ -1,68 +1,49 @@
-"use client"
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 import ProductCard from "@/Components/Home/Product/ProductCard";
 import AllProductSkeleton from "@/Components/Skeleton/AllProductSkeleton";
 import { ProductsURL } from "@/helper/allLinks";
-import Spinner from "@/Components/Home/Utility/Spinner";
+import axios from "axios";
+import React from "react";
+import { Suspense } from "react";
 
-const AllProductComC = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await axios.get(ProductsURL);
-        const { products } = res.data;
-        setProducts(products || []);
-      } catch (err) {
-        setError("Error occurred while fetching products");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-  if (loading) {
+const AllProductComC = async () => {
+  const res = await axios.get(ProductsURL, {
+    headers: {
+      "Cache-Control": "no-cache",
+    },
+  });
+  const { products } = await res?.data;
+  if (!products) {
     return (
-      <div className=" fixed left-0  h-screen w-full grid place-items-center bg-gray-950">
-        <Spinner/>
+      <div className="h-screen w-full grid place-items-center   bg-gray-950  ">
+        Error occuured
       </div>
     );
   }
-
-  if (error) {
-    return (
-      <div className="h-screen w-full grid place-items-center bg-gray-950">
-        {error}
-      </div>
-    );
-  }
-
   return (
     <div>
-      {products.length === 0 ? (
-        <div className="w-full text-center font-semibold">No Products Found</div>
-      ) : (
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
-          {products.map((item, index) => (
-            <ProductCard
-              key={index}
-              description={item.description}
-              thumbnail={item.thumbnail}
-              title={item.title}
-              price={item.pricing.price}
-              comPrice={item.pricing.comAtPrice}
-              views={item.views}
-              id={item._id}
-            />
-          ))}
+      {products?.length === 0 && (
+        <div className="w-full text-center font-semibold ">
+          No Products Found
         </div>
       )}
+      <Suspense fallback={<AllProductSkeleton />}>
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-4 ">
+          {products?.map((item, index) => {
+            return (
+              <ProductCard
+                key={index}
+                description={item?.description}
+                thumbnail={item?.thumbnail}
+                title={item?.title}
+                price={item?.pricing?.price}
+                comPrice={item?.pricing?.comAtPrice}
+                views={item?.views}
+                id={item?._id}
+              />
+            );
+          })}
+        </div>
+      </Suspense>
     </div>
   );
 };
